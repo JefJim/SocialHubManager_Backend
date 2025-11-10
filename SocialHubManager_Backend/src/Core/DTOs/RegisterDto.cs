fe96efb753e8551431e5479df52b2f06cb1b6ed6
JefJim <jefryjimenez2011@gmail.com>
2025-11-10 09:28:15 -0600

    Add LoginDto and RegisterDto classes for user data
    Introduced two new DTO classes, `LoginDto` and `RegisterDto`, in the `SocialHubManager_Backend.src.Core.DTOs` namespace.
    
    - `LoginDto` includes `Email` and `Password` properties for user authentication.
    - `RegisterDto` includes `Username`, `Email`, and `Password` properties for user registration.
    
    These changes improve data encapsulation and streamline the transfer of authentication and registration data between the client and server.

---
+++ SocialHubManager_Backend/src/Core/DTOs/RegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SocialHubManager_Backend.src.Core.DTOs
+{
+    public class RegisterDto
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
