5e682a7d695df2eeab0b0ca7ccb5e12f9dc84638
JefJim <jefryjimenez2011@gmail.com>
2025-11-12 15:52:25 -0600

    Referencing with each column properly

--- SocialHubManager_Backend/src/Core/Entities/UserSocialNetwork.cs
+++ SocialHubManager_Backend/src/Core/Entities/UserSocialNetwork.cs
@@ -1,11 +1,39 @@
-﻿namespace SocialHubManager_Backend.src.Core.Entities
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SocialHubManager_Backend.src.Core.Entities
 {
+    [Table("SocialAccounts")] 
     public class UserSocialNetwork
     {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("userId")] 
         public int UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
         public User User { get; set; } = null!;
 
-        public int SocialNetworkId { get; set; }
-        public SocialNetwork SocialNetwork { get; set; } = null!;
+        [Column("provider")]
+        public string Provider { get; set; } = null!;
+        [Column("providerId")]
+        public string ProviderId { get; set; } = null!;
+        [Column("token")]
+        public string Token { get; set; } = null!;
+        [Column("refreshToken")]
+        public string? RefreshToken { get; set; }
+        [Column("displayName")]
+        public string? DisplayName { get; set; }
+        [Column("clientId")]
+        public string? ClientId { get; set; }
+        [Column("clientSecret")]
+        public string? ClientSecret { get; set; }
+        [Column("createdAt")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        [Column("updatedAt")]
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     }
 }
