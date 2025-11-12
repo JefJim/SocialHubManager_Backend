33d8819027942da9110cb8b18b2e034bd17fac06
JefJim <jefryjimenez2011@gmail.com>
2025-11-12 15:51:28 -0600

    Adding new scopes for 2fa verification

--- SocialHubManager_Backend/src/WebAPI/Program.cs
+++ SocialHubManager_Backend/src/WebAPI/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using SocialHubManager_Backend.src.Application;
+using SocialHubManager_Backend.src.Core.Interfaces;
 using SocialHubManager_Backend.src.Infrastructure.Data;
+using SocialHubManager_Backend.src.Infrastructure.Services;
+using System.Text;
+using TwoFactorService = SocialHubManager_Backend.src.Application.TwoFactorService;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +21,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 // Registro de servicios
 // ---------------------
 builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<TwoFactorService>();
 
 // ---------------------
 // CORS para Angular
@@ -41,22 +50,51 @@ builder.Services.AddSwaggerGen();
 // ---------------------
 builder.Services.AddControllers();
 
-var app = builder.Build();
+// ---------------------
+// Authentication/Authorization
+// ---------------------
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"] ?? "SuperSecretKey"))
+    };
+});
+builder.Services.AddAuthorization();
+
+ var app = builder.Build();
 
 // ---------------------
 // Middleware
 // ---------------------
-app.UseCors(MyAllowSpecificOrigins);
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// Habilitar CORS antes de Authorization
+app.UseCors(MyAllowSpecificOrigins);
+
 app.UseHttpsRedirection();
 
+// Habilitar autenticación/authorization
+app.UseAuthentication();
 app.UseAuthorization();
+
+// JWT Middleware personalizado
+app.UseMiddleware<WebAPI.Middleware.JwtMiddleware>();
 
 app.MapControllers();
 
