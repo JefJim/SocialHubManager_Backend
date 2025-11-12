473a785b18273c19760d1e78349d85d08f7760dd
JefJim <jefryjimenez2011@gmail.com>
2025-11-12 15:54:39 -0600

    TwoFactorServices with method "VerifyCode" to verify 2fa code

---
+++ SocialHubManager_Backend/src/Application/TwoFactorService.cs
@@ -0,0 +1,18 @@
+﻿using SocialHubManager_Backend.src.Core.Entities;
+using OtpNet;
+
+namespace SocialHubManager_Backend.src.Application
+{
+    public class TwoFactorService
+    {
+        public bool VerifyCode(User user, string token)
+        {
+            if (string.IsNullOrEmpty(user.OtpSecret))
+                return false;
+
+            var secretBytes = Base32Encoding.ToBytes(user.OtpSecret);
+            var totp = new Totp(secretBytes);
+            return totp.VerifyTotp(token, out _, VerificationWindow.RfcSpecifiedNetworkDelay);
+        }
+    }
+}
